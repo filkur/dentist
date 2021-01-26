0eb9082f4f4c18624391492de0936125aa52b8b4
filkur <filipkuranda@gmail.com>
2021-01-26 21:53:52 +0100

    cancel visit

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -8,12 +8,13 @@
                 <h3>Zarezerwowane wizyty:</h3>
                 <hr>
                 <div class="row mt-4 ml-1" >
-                    <div class="col"><h5>#Id<br> rezerwacji:</h5></div>
-                    <div class="col"><h5>#Id <br> klienta:</h5></div>
-                    <div class="col"><h5>Specjalista:</h5></div>
-                    <div class="col"><h5>Typ wizyty:</h5></div>
-                    <div class="col"><h5>Data:</h5></div>
-                    <div class="col"><h5>Godzina:</h5></div>
+                    <div class="col ml-0"><h5>#Id<br> rezerwacji:</h5></div>
+                    <div class="col ml-4"><h5>#Id <br> klienta:</h5></div>
+                    <div class="col ml-5"><h5>Specjalista:</h5></div>
+                    <div class="col ml-5"><h5>Typ wizyty:</h5></div>
+                    <div class="col ml-5"><h5>Data:</h5></div>
+                    <div class="col ml-5"><h5>Godzina:</h5></div>
+                    <div class="col ml-5"><h5>-</h5></div>
                 </div>
             </div>
 
@@ -26,7 +27,7 @@
                             @endforeach
                                 <form action="/historia/{{\Illuminate\Support\Facades\Auth::id()}}/{{$visit -> id}}" method="post">
                                     @csrf
-                                    <a href="{{route('historyDes', ['userId'=>Auth::id(),'visitId'=>$visit -> id])}}" class="btn btn-primary btn-sm" >Anuluj</a>
+                                    <a href="{{route('historyDes', ['userId'=>Auth::id(),'visitId'=>$visit -> id])}}" class="btn btn-warning btn-sm" >Anuluj</a>
                                 </form>
                         </div>
                     @endforeach
